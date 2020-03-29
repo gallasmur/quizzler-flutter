d5a189f0d75cd959a0b31db2091454c652550b18
Jose <jrabal.segura@gmail.com>
2020-03-29 16:42:12 +0200

    Added question class and reformated code with it.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'question.dart';
+
 void main() => runApp(Quizzler());
 
 class Quizzler extends StatelessWidget {
@@ -26,12 +28,15 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Widget> scoreKeeper = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
+
+  List<Question> questions = [
+    new Question(
+        t: 'You can lead a cow down stairs but not up stairs.', a: false),
+    new Question(
+        t: 'Approximately one quarter of human bones are in the feet.',
+        a: true),
+    new Question(t: 'A slug\'s blood is green.', a: true)
   ];
-  List<bool> answers = [false, true, true];
 
   int contador = 0;
 
@@ -47,7 +52,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[contador],
+                questions[contador].text,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -72,7 +77,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  if (answers[contador] == true) {
+                  if (questions[contador].answer == true) {
                     scoreKeeper.add(Icon(
                       Icons.check,
                       color: Colors.green,
@@ -103,7 +108,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 setState(() {
-                  if (answers[contador] == false) {
+                  if (questions[contador].answer == false) {
                     scoreKeeper.add(Icon(
                       Icons.check,
                       color: Colors.green,
